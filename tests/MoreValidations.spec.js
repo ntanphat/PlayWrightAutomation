584afa39a777973254ad149a94e6d46cc1ffbd4d
Felix Nguyen <nguyentanphat1304@gmail.com>
2025-03-31 17:20:46 +0700

    L42

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,8 @@
+const { test, expect} = require('@playwright/test');
+
+test("Popup Validations", async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await page.goto("https://google.com");
+    await page.goBack();
+    await page.goForward();
+})
