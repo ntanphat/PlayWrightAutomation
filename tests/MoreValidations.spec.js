332b74ed12bb3c23ab97155544f242b21e1787e5
Phat Nguyen <nguyentanphat1304@gmail.com>
2025-04-01 05:49:08 +0700

    L43

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -2,7 +2,15 @@ const { test, expect} = require('@playwright/test');
 
 test("Popup Validations", async ({page})=>{
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-    await page.goto("https://google.com");
-    await page.goBack();
-    await page.goForward();
+    // await page.goto("https://google.com");
+    // await page.goBack();
+    // await page.goForward();
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    await page.pause();
+    //await page.locator("#confirmbtn").click();
+    page.on("dialog",dialog => dialog.accept());
+    await page.locator("#confirmbtn").click(); 
+    await page.locator("#mousehover").hover();
 })
